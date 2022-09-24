90f37aef5dd45bba555cd93153706545237b278f
Ranjeetkumarlpu <84727061+Ranjeetkumarlpu@users.noreply.github.com>
2022-09-24 16:13:47 +0530

    Update login.php

--- login.php
+++ login.php
@@ -9,14 +9,14 @@ session_start();
 	if($_SERVER['REQUEST_METHOD'] == "POST")
 	{
 		//something was posted
-		$user_name = $_POST['user_name'];
+		$admin_name = $_POST['admin_name'];
 		$password = $_POST['password'];
 
-		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		if(!empty($admin_name) && !empty($password) && !is_numeric($admin_name))
 		{
 
 			//read from database
-			$query = "select * from users where user_name = '$user_name' limit 1";
+			$query = "select * from admin where admin_name = '$admin_name' limit 1";
 			$result = mysqli_query($con, $query);
 
 			if($result)
@@ -24,12 +24,12 @@ session_start();
 				if($result && mysqli_num_rows($result) > 0)
 				{
 
-					$user_data = mysqli_fetch_assoc($result);
+					$admin_data = mysqli_fetch_assoc($result);
 					
-					if($user_data['password'] === $password)
+					if($admin_data['password'] === $password)
 					{
 
-						$_SESSION['user_id'] = $user_data['user_id'];
+						$_SESSION['admin_id'] = $admin_data['admin_id'];
 						header("Location: index.php");
 						die;
 					}
@@ -39,62 +39,207 @@ session_start();
 			echo "wrong username or password!";
 		}else
 		{
+		    
 			echo "wrong username or password!";
+			
 		}
 	}
 
 ?>
-
-
-<!DOCTYPE html>
 <html>
-<head>
-	<title>Login</title>
-</head>
-<body>
-<center><h1>Welcome To MoveBold Admin Panel</h1></center>
-	<style type="text/css">
-	
-	#text{
+    <body>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<div class="login-box">
+  <h2>Admin Login</h2>
+  <form method="post">
+    <div class="user-box">
+      <input type="text" name="admin_name" required="">
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="password" required="">
+      <label>Password</label>
+    </div>
+   <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <input id="button" type="submit" value="login"><br><br>
+    
+    
+  </form>
+</div>
 
-		height: 25px;
-		border-radius: 5px;
-		padding: 4px;
-		border: solid thin #aaa;
-		width: 100%;
-	}
+<style>
+  html {
+  height: 100%;
+}
+body {
+  margin:0;
+  padding:0;
+  font-family: sans-serif;
+  background: linear-gradient(#141e30, #243b55);
+}
 
-	#button{
+.login-box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  padding: 40px;
+  transform: translate(-50%, -50%);
+  background: rgba(0,0,0,.5);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0,0,0,.6);
+  border-radius: 10px;
+}
 
-		padding: 10px;
-		width: 100px;
-		color: white;
-		background-color: lightblue;
-		border: none;
-	}
+.login-box h2 {
+  margin: 0 0 30px;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+}
 
-	#box{
+.login-box .user-box {
+  position: relative;
+}
 
-		background-color: grey;
-		margin: auto;
-		width: 300px;
-		padding: 20px;
-	}
+.login-box .user-box input {
+  width: 100%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  background: transparent;
+}
+.login-box .user-box label {
+  position: absolute;
+  top:0;
+  left: 0;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  pointer-events: none;
+  transition: .5s;
+}
 
-	</style>
+.login-box .user-box input:focus ~ label,
+.login-box .user-box input:valid ~ label {
+  top: -20px;
+  left: 0;
+  color: #03e9f4;
+  font-size: 12px;
+}
 
-	<div id="box">
-		
-		<form method="post">
-			<div style="font-size: 20px;margin: 10px;color: white;">Login</div>
+#button {
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  color: #03e9f4;
+  font-size: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  overflow: hidden;
+  transition: .5s;
+  margin-top: 40px;
+  letter-spacing: 4px
+}
 
-			<input id="text" type="text" name="user_name"><br><br>
-			<input id="text" type="password" name="password"><br><br>
+#button:hover {
+  background: #03e9f4;
+  color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 5px #03e9f4,
+              0 0 25px #03e9f4,
+              0 0 50px #03e9f4,
+              0 0 100px #03e9f4;
+}
 
-			<input id="button" type="submit" value="Login"><br><br>
+.login-box a span {
+  position: absolute;
+  display: block;
+}
 
-			<!--<a href="signup.php">Click to Signup</a><br><br>-->
-		</form>
-	</div>
+.login-box a span:nth-child(1) {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, #03e9f4);
+  animation: btn-anim1 1s linear infinite;
+}
+
+@keyframes btn-anim1 {
+  0% {
+    left: -100%;
+  }
+  50%,100% {
+    left: 100%;
+  }
+}
+
+.login-box a span:nth-child(2) {
+  top: -100%;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(180deg, transparent, #03e9f4);
+  animation: btn-anim2 1s linear infinite;
+  animation-delay: .25s
+}
+
+@keyframes btn-anim2 {
+  0% {
+    top: -100%;
+  }
+  50%,100% {
+    top: 100%;
+  }
+}
+
+.login-box a span:nth-child(3) {
+  bottom: 0;
+  right: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(270deg, transparent, #03e9f4);
+  animation: btn-anim3 1s linear infinite;
+  animation-delay: .5s
+}
+
+@keyframes btn-anim3 {
+  0% {
+    right: -100%;
+  }
+  50%,100% {
+    right: 100%;
+  }
+}
+
+.login-box a span:nth-child(4) {
+  bottom: -100%;
+  left: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(360deg, transparent, #03e9f4);
+  animation: btn-anim4 1s linear infinite;
+  animation-delay: .75s
+}
+
+@keyframes btn-anim4 {
+  0% {
+    bottom: -100%;
+  }
+  50%,100% {
+    bottom: 100%;
+  }
+}
+
+
+</style>
 </body>
 </html>
